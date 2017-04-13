3ff1e8fbc2cc3144d8c2bfe94defa96300719297
Mohith G <mohithgm@gmail.com>
2017-04-13 18:07:06 +0530

    Added documentation

--- helpers.js
+++ helpers.js
@@ -1,8 +1,17 @@
-export const wait = (selector, index) => {
+/**
+ * function wait()
+ * Accepts selector to wait and scroll to that position
+ */
+export const wait = (selector) => {
 	browser.waitForExist(selector);
   browser.scroll(selector);
 }
 
+/**
+ * function click()
+ * Accepts selector i.e., element to click
+ * Accepts index (default 0) to click on element if there is multiple in page.
+ */
 export const click = (selector, index = 0) => {
 	wait(selector, index);
 	$$(selector)[index].click();
